5afa5c8a91d5bd1631f354398698bc7c8ec47c91
Himanshu <hgusain088@gmail.com>
2024-03-04 12:01:24 +0530

    com

--- server/index.js
+++ server/index.js
@@ -14,8 +14,9 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    credentials: true,
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173",
+    methods: ["POST", "GET"],
+    credentials: true
   })
 )
 
